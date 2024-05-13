2015e66572ce5eb6987efec9bf51ec3acc66577e
Purvesh Krishnani <purvesh152001@gmail.com>
2024-05-14 04:26:13 +0530

    apigateway

--- UserService/src/main/java/com/RestaurantsRatingApplication/UserService/external/RatingService.java
+++ UserService/src/main/java/com/RestaurantsRatingApplication/UserService/external/RatingService.java
@@ -4,10 +4,7 @@ import com.RestaurantsRatingApplication.UserService.entity.Rating;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @Service
 @FeignClient(name = "RatingService")
